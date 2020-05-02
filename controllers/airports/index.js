a9fab5f09c5d2dfa19b1217d837920fe2af365bb
Nayara Ferreira <ncy.fers@gmail.com>
2020-05-01 21:08:46 -0300

    ajuste na rota de simulacoes do controlador de aeroporto para busca assíncrona

--- controllers/airports/index.js
+++ controllers/airports/index.js
@@ -2,6 +2,7 @@
 //Rota de Aeroportos
 const Airport = require('../../models/airports');
 const async = require('async');
+const montaCalculo = require('../../functions/montaCaulculo');
 
 module.exports = function (router) {
 
@@ -23,129 +24,18 @@ module.exports = function (router) {
         return res.json({ sucess: `Aeroporto ${airports.name} deletado com sucesso!` });
     });
 
-    router.get('/test', (req, res) => {
+    router.get('/simulacao', async function (req, res) {
         const aeroporto = {
-            qtdAeroportos: 4,
+            qtdAeroportos: 3,
             largura: 10,
             altura: 10,
             qtdNuvens: 4,
         }
-
-        const matriz = [];
-
-
-        for(let y = 0; y < aeroporto.altura; y++){
-            const eixoX = [];
-            for(let x = 0; x < aeroporto.largura; x++){
-                eixoX.push({
-                    isAeroporto: false,
-                    isNuvem: false,
-                })
-            }
-            matriz.push(eixoX);
-        }
         
+        const simular = await montaCalculo(aeroporto);
 
+        return res.json({ success: true, data: simular});
 
-
-
-        for(let i = 0; i < aeroporto.qtdAeroportos; i++){
-            let foiAtribuido = false;
-            do{
-                const x = Math.floor(Math.random() * aeroporto.largura);
-                const y = Math.floor(Math.random() * aeroporto.altura);
-
-                if( !matriz[y][x].isAeroporto &&  ! matriz[y][x].isNuvem){
-                    matriz[y][x].isAeroporto = true;
-                    foiAtribuido = true;
-                }
-            }while(!foiAtribuido );
-        }
-
-
-        for(let i = 0; i < aeroporto.qtdNuvens; i++){
-            let foiAtribuido = false;
-            do{
-                const x = Math.floor(Math.random() * aeroporto.largura);
-                const y = Math.floor(Math.random() * aeroporto.altura);
-
-                if( !matriz[y][x].isAeroporto &&  ! matriz[y][x].isNuvem){
-                    matriz[y][x].isNuvem = true;
-                    foiAtribuido = true;
-                }
-            }while(!foiAtribuido );
-        }
-
-        //save matriz; 
-
-        const simulacoes = [
-            {
-                dia: 1,
-                matriz: JSON.parse(JSON.stringify(matriz)),
-            }
-        ];
-
-        let possuiAeroportoSemNuvens = true;
-
-        let dia = 1;
-        do{
-            dia ++;
-            
-            for(const [indexEixoY, eixoY] of matriz.entries()){
-                for(const [indexEixoX, eixoX] of eixoY.entries()){
-                    if(eixoX.isNuvem && eixoX.createAt !== dia){
-
-                        //horizontal a esquerda
-                        if(matriz[indexEixoY][indexEixoX - 1] && !matriz[indexEixoY][indexEixoX - 1].isNuvem){
-                            matriz[indexEixoY][indexEixoX - 1].isNuvem = true
-                            matriz[indexEixoY][indexEixoX - 1].createAt = dia;
-                        }
-
-                        //horizontal a direita
-                        if(matriz[indexEixoY][indexEixoX + 1] && !matriz[indexEixoY][indexEixoX + 1].isNuvem ){
-                            matriz[indexEixoY][indexEixoX + 1].isNuvem = true;
-                            matriz[indexEixoY][indexEixoX + 1].createAt = dia;
-                        }
-
-                        //vertical para baixo
-                        if(matriz[indexEixoY - 1] && !matriz[indexEixoY - 1][indexEixoX].isNuvem){
-                            matriz[indexEixoY - 1][indexEixoX].isNuvem = true;
-                            matriz[indexEixoY - 1][indexEixoX].createAt = dia;
-                        }
-
-                        //vertical para cima
-                        if(matriz[indexEixoY + 1] &&  !matriz[indexEixoY + 1][indexEixoX].isNuvem){
-                            matriz[indexEixoY + 1][indexEixoX].isNuvem = true;
-                            matriz[indexEixoY + 1][indexEixoX].createAt = dia;
-                        }
-                    }
-                }
-            }
-
-
-            simulacoes.push({
-                dia,
-                matriz: JSON.parse(JSON.stringify(matriz))
-            })
-
-            possuiAeroportoSemNuvens = false;
-
-            for(const [y, eixoY] of matriz.entries()){
-                for(const [x, eixoX] of eixoY.entries()){
-                   
-                    if(eixoX.isAeroporto && !eixoX.isNuvem){
-                        console.log(matriz, eixoX, x, y, dia);
-                        possuiAeroportoSemNuvens = true;
-                    }
-                }
-            }
-
-        }while(possuiAeroportoSemNuvens );
-
-
-
-        res.json({ success: true, data: simulacoes});
-        
     })
 };
 
