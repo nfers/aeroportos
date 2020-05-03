cbfb64628fde19144732637e932d2bfe679ba4ee
Nayara Ferreira <ncy.fers@gmail.com>
2020-05-02 21:05:17 -0300

    ajuste para retornar á página inical quando a rota informada nao existir

--- airport-ui/src/index.js
+++ airport-ui/src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={App} />
-        
+        <Route path="/*" exact component={App} />
       </Switch>
     </ BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
